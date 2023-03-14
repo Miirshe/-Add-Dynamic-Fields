494f3264bf5551be3901b6b40bc2a5ee43926261
Miirshe <126691024+Miirshe@users.noreply.github.com>
2023-03-14 14:57:08 +0300

    Add files via upload
    dynamic fields input that has two of button one add field and the other is revomed fields

---
+++ input.js
@@ -0,0 +1,25 @@
+// var add_fields = document.getElementById('add_fields');
+// var remove_fields = document.getElementById('remove_fields');
+// var options = document.getElementById('surveys_options');
+
+// add_fields.addEventListener('click',function(){
+//     let newFields = document.createElement('input');
+//     newFields.type='text';
+//     newFields.name='surveys_options[]';
+//     newFields.placeholder='add fields';
+//     options.appendChild(newFields);
+// })
+
+// remove_fields.addEventListener('click',function(){
+//     let removes = options.getElementsByTagName('input');
+//     if(removes.length>2){
+//         options.removeChild(removes[removes.length-1]);
+//     }
+//     else{
+//         alert('fadlan 2 input wax kabadan madaleedi doontid');
+//     }
+// })
+
+
+
+
